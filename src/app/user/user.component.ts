c49075f7039c62280f62917d182a1efc691eb240
Dineshkumar <dinesh.athiappan@outlook.com>
2024-08-29 18:30:17 +0530

    User Addition, data binding, managing state

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal , computed} from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+
+//It will give u a number between 0 to dummy user length
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +14,21 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+
+  //Change detection mechanism using SIGNALS
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imageUrl = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // get imageUrl() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser()
+  {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+
 
 }
