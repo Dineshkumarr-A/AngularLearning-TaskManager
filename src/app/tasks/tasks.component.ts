3ec90df3fd19f5bc6420087dc58ca34e4d185555
Dineshkumar <dinesh.athiappan@outlook.com>
2024-09-02 08:55:52 +0530

    form submit and shared component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,19 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  @Input({ required: true }) name!: string;
 
-  @Input({required: true}) name!: string;
-
-  @Input({required: true}) userId!: string;
+  @Input({ required: true }) userId!: string;
 
   isAddingTask = false;
 
@@ -41,16 +41,32 @@ export class TasksComponent {
         'Prepare and describe an issue template which will help with project management',
       dueDate: '2024-06-15',
     },
-  ]
+  ];
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId == this.userId)
+    return this.tasks.filter((task) => task.userId == this.userId);
   }
-  onCompleteTask(taskId: string)
-  {
+  onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id != taskId);
   }
   onStartAddTask() {
     this.isAddingTask = true;
-    }
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData)
+  {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+
+    this.isAddingTask = false
+  }
 }
